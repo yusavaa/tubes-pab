1e15a34611d5768337778227a46cc5a967545304
Alvhsyv <126624892+Alvhsyv@users.noreply.github.com>
2024-12-09 13:08:32 +0700

    ini dari ncip tampilan notif dan article

---
+++ app/src/main/java/com/example/tubespab/NotificationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tubespab
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
+
+class NotificationActivity {
+}
